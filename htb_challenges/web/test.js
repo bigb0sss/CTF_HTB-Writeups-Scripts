464e61cbc85b6ee24cd4c812982ded80eb5e2657
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2022-08-07 18:53:16 -0400

    Delete test.js

--- htb_challenges/web/test.js
+++
@@ -1,8 +0,0 @@
-// const isLocalhost = req => ((req.ip == '127.0.0.1' && req.headers.host == '127.0.0.1:1337') ? 0 : 1);
-
-// console.log(isLocalhost)
-
-var sc = document.createElement("script");
-sc.setAttribute("src", "https://getfirebug.com/firebug-lite.js");
-sc.setAttribute("type", "text/javascript");
-document.head.appendChild(sc);
