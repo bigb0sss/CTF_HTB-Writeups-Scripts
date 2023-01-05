ac32f22d911c66dd63b9462528cd00483d89d184
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2023-01-04 22:25:03 -0500

    Create easterbunny.py

---
+++ htb_challenges/web/easterbunny.py
@@ -0,0 +1,10 @@
+# EasterBunney
+
+# Description
+# It's that time of the year again! Write a letter to the Easter bunny and make your wish come true! But be careful what you wish for because the Easter bunny's helpers are watching!
+
+# Vulnerability
+
+
+import requests
+
